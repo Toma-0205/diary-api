8102869619923548a1efb9479c0b00c870f78bad
Toma Morishita <25winterhorse@gmail.com>
2025-06-08 17:39:25 +0900

    AWS_RDS接続成功（残：空DBがフロント側で表示されている）

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,53 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { DiaryEntry } from './diary-entry/entities/diary-entry.entity';
 import { DiaryEntryModule } from './diary-entry/diary-entry.module';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-// デコレーター（@Module）でNestJSにモジュールであると宣言
+
 @Module({
-  // TypeORMを使用してSQLiteに接続
   imports: [
-    TypeOrmModule.forRoot({
-      // 使用するDB
-      type: 'sqlite',
-      // 作成するファイル名
-      database: 'db.sqlite',
-      // エンティティ
-      entities: [DiaryEntry],
-      synchronize: true, // 開発中のみtrueで自動生成。（本番はfalseにするよ！！！！）
+    // .env を読み込む
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+
+    // PostgreSQL 設定（環境変数ベース）
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService): TypeOrmModuleOptions => {
+        const dbConfig: TypeOrmModuleOptions = {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          port: config.get<number>('DB_PORT'),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_DATABASE'),
+          entities: [DiaryEntry],
+          synchronize: true,
+          ssl: true,
+          extra: {
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          },
+        };
+      
+        console.log('Connecting to DB:', dbConfig);
+        return dbConfig;
+      },      
+    }),
+
+    
+
     DiaryEntryModule,
   ],
-
   controllers: [AppController],
   providers: [AppService],
-  
 })
 export class AppModule {}
